75387e76e1fc0c0f56aa87bde4b70e12c8c34d9c
PabloVLS <viniciuspablo801@gmail.com>
2025-02-19 17:16:53 -0300

    Feito Menu

--- feedback-anonimo/src/Home/App.js
+++ feedback-anonimo/src/Home/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import logo1 from './grupo_lidon_logo.png';
+
+
 
 function App() {
   return (
-    <div className="App">
+<div className="App">
+  <div className="menu-bar">
+    <img src={logo1} className="img-grupoLidon" alt="logoGrupoLidon" />
+    <div className='title-menu'>
+      <p>Feedback-Anonimo</p>
+    </div>
+    
+  </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
